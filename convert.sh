f2bff20174ddbf2a7a7c9cdb2baaa2b2f0fec662
ringsaturn <ringsaturn.me@gmail.com>
2024-01-13 13:54:33 +0800

    cleanup

--- convert.sh
+++
@@ -1,1 +0,0 @@
-gdalwarp -t_srs EPSG:3857  -dstnodata None  -novshiftgrid -co TILED=YES  -co COMPRESS=DEFLATE  -co BIGTIFF=IF_NEEDED -r lanczos ASTGTMV003_N27E086_dem.tif  ASTGTMV003_N27E086_dem_EPSG3857.tif
